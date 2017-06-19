f6944cf49ee2f5ea605b533a916b47755f0eacc5
Chris Womack <chris.d.womack@gmail.com>
2017-06-19 08:34:49 -0600

    Change up

--- web/index.php
+++ web/index.php
@@ -1,72 +1,17 @@
-<?php
-function display()
-{
-    //if (!empty($_GET['location'])) {
-        /**
-         * Here we build the url we'll be using to access the google maps api
-         * urlencode will encode any invalid characters that users input i.e. whitespace
-         */
-        $maps_url =
-            'https://' .
-            'maps.googleapis.com/' .
-            'maps/api/geocode/json' .
-            '?address=' . urlencode($_GET['location']);
-
-        /* Processes the "GET" request and returns json. Note there are other methods for this function
-        * i.e. PUT POST
-        */
-        $maps_json = file_get_contents($maps_url);
-
-        // Now decode the json returned from the GET request into an array for users
-        $maps_array = json_decode($maps_json, true);
-
-        // Lets get the lat and long for the instagram api
-        $lat = $maps_array['results'][0]['geometry']['location']['lat'];
-        $lng = $maps_array['results'][0]['geometry']['location']['lng'];
-
-        /**
-         * Time to make our Instagram api request. We'll build the url using the
-         * coordinate values returned by the google maps api
-         */
-        $insta_url =
-            'http://api.openweathermap.org/data/2.5/forecast' .
-            '?lat=' . $lat .
-            '&lng=' . $lng .
-            '&client_id=812c81cd96b946c7a36d14d3373e85cc'; //replace "CLIENT-ID"
-
-        // get the json from the api call
-        $weather_json = file_get_contents($url);
-
-        // convert json into array
-        $weather_array = json_decode($json, true);
-    //}
+<?php 
+if($_POST['logme']){
+if(!empty($_POST['username']) AND !empty($_POST['pass'])){
+echo "Your Are Logged $_POST[username] ";
+}else{
+echo "You have to fill out the two fields !";
 }
-
-if(isset($_POST['SubmitButton']))
-{
-       display();
-}
+}else{
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8"/>
-    <title>geogram</title>
-</head>
-<body>
-<form action="#" method="post">
-    <input type="text" name="location"/>
-    <button type="submit" name="SubmitButton">Submit</button>
-    <br>
-    <?php
-        echo $weather_array;
-        if(!empty($weather_array))
-        {
-            echo '<p>'.$weather_array['list']['main']['temp'].'</p>';
-            echo $weather_array;
-            echo '<br>';
-        }
-    ?>
+<form method="post">
+Username : <input type="text" name="username" />
+Password: <input type="password" name="pass" />
+<br/>
+<input type="submit" name="logme" value="Login" />
 </form>
-</body>
-</html>
+<?php
+}
