d8fcd97b23a4c760659603fe13276352fac67bd7
Chris Womack <chris.d.womack@gmail.com>
2017-06-19 07:29:46 -0600

    Change up

--- web/index.php
+++ web/index.php
@@ -2,26 +2,41 @@
 if (!empty($_GET['location'])) {
     /**
      * Here we build the url we'll be using to access the google maps api
+     * urlencode will encode any invalid characters that users input i.e. whitespace
      */
-    $maps_url = 'https://' .
+    $maps_url =
+        'https://' .
         'maps.googleapis.com/' .
         'maps/api/geocode/json' .
         '?address=' . urlencode($_GET['location']);
+
+    /* Processes the "GET" request and returns json. Note there are other methods for this function
+    * i.e. PUT POST
+    */
     $maps_json = file_get_contents($maps_url);
+
+    // Now decode the json returned from the GET request into an array for users
     $maps_array = json_decode($maps_json, true);
+
+    // Lets get the lat and long for the instagram api
     $lat = $maps_array['results'][0]['geometry']['location']['lat'];
     $lng = $maps_array['results'][0]['geometry']['location']['lng'];
+
     /**
      * Time to make our Instagram api request. We'll build the url using the
      * coordinate values returned by the google maps api
      */
-    $insta_url = 'https://' .
-        'api.instagram.com/v1/media/search' .
+    $insta_url =
+        'http://api.openweathermap.org/data/2.5/forecast' .
         '?lat=' . $lat .
         '&lng=' . $lng .
         '&client_id=812c81cd96b946c7a36d14d3373e85cc'; //replace "CLIENT-ID"
-    $insta_json = file_get_contents($url);
-    $insta_array = json_decode($json, true);
+
+    // get the json from the api call
+    $weather_json = file_get_contents($url);
+
+    // convert json into array
+    $weather_array = json_decode($json, true);
 }
 ?>
 <!DOCTYPE html>
@@ -35,13 +50,11 @@ if (!empty($_GET['location'])) {
     <input type="text" name="location"/>
     <button type="submit">Submit</button>
     <?php
-    //if (!empty($insta_array)) {
-        foreach ($insta_array['data'] as $image) {
-            echo '<img src="'
-            .$image['images']['low_resolution']['url'].'"
-            alt=""/><br/>';
+        if(!empty($weather_array))
+        {
+            echo '<img src="'.$weather_array['list']['main']['temp'].'" alt=""/>';
+            echo '<br>';
         }
-    //}
     ?>
 </form>
 </body>
