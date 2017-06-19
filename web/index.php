61667ad01583d3953ec808e8c9a377775dc30f8b
Chris Womack <chris.d.womack@gmail.com>
2017-06-19 08:19:40 -0600

    Change up

--- web/index.php
+++ web/index.php
@@ -1,42 +1,50 @@
 <?php
-if (!empty($_GET['location'])) {
-    /**
-     * Here we build the url we'll be using to access the google maps api
-     * urlencode will encode any invalid characters that users input i.e. whitespace
-     */
-    $maps_url =
-        'https://' .
-        'maps.googleapis.com/' .
-        'maps/api/geocode/json' .
-        '?address=' . urlencode($_GET['location']);
+function display()
+{
+    //if (!empty($_GET['location'])) {
+        /**
+         * Here we build the url we'll be using to access the google maps api
+         * urlencode will encode any invalid characters that users input i.e. whitespace
+         */
+        $maps_url =
+            'https://' .
+            'maps.googleapis.com/' .
+            'maps/api/geocode/json' .
+            '?address=' . urlencode($_GET['location']);
 
-    /* Processes the "GET" request and returns json. Note there are other methods for this function
-    * i.e. PUT POST
-    */
-    $maps_json = file_get_contents($maps_url);
+        /* Processes the "GET" request and returns json. Note there are other methods for this function
+        * i.e. PUT POST
+        */
+        $maps_json = file_get_contents($maps_url);
 
-    // Now decode the json returned from the GET request into an array for users
-    $maps_array = json_decode($maps_json, true);
+        // Now decode the json returned from the GET request into an array for users
+        $maps_array = json_decode($maps_json, true);
 
-    // Lets get the lat and long for the instagram api
-    $lat = $maps_array['results'][0]['geometry']['location']['lat'];
-    $lng = $maps_array['results'][0]['geometry']['location']['lng'];
+        // Lets get the lat and long for the instagram api
+        $lat = $maps_array['results'][0]['geometry']['location']['lat'];
+        $lng = $maps_array['results'][0]['geometry']['location']['lng'];
 
-    /**
-     * Time to make our Instagram api request. We'll build the url using the
-     * coordinate values returned by the google maps api
-     */
-    $insta_url =
-        'http://api.openweathermap.org/data/2.5/forecast' .
-        '?lat=' . $lat .
-        '&lng=' . $lng .
-        '&client_id=812c81cd96b946c7a36d14d3373e85cc'; //replace "CLIENT-ID"
+        /**
+         * Time to make our Instagram api request. We'll build the url using the
+         * coordinate values returned by the google maps api
+         */
+        $insta_url =
+            'http://api.openweathermap.org/data/2.5/forecast' .
+            '?lat=' . $lat .
+            '&lng=' . $lng .
+            '&client_id=812c81cd96b946c7a36d14d3373e85cc'; //replace "CLIENT-ID"
 
-    // get the json from the api call
-    $weather_json = file_get_contents($url);
+        // get the json from the api call
+        $weather_json = file_get_contents($url);
 
-    // convert json into array
-    $weather_array = json_decode($json, true);
+        // convert json into array
+        $weather_array = json_decode($json, true);
+    //}
+}
+
+if(isset($_GET['location']))
+{
+       display();
 }
 ?>
 <!DOCTYPE html>
@@ -48,7 +56,7 @@ if (!empty($_GET['location'])) {
 <body>
 <form action="" method="get">
     <input type="text" name="location"/>
-    <button type="submit">Submit</button>
+    <button type="submit" value="click" name="submit">Submit</button>
     <br>
     <?php
         echo $weather_array;
