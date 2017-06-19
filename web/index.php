d23a2226335c7a073b0ddd863ada2a0ac0d26797
Chris Womack <chris.d.womack@gmail.com>
2017-06-18 23:30:51 -0600

    added REST API to form

--- web/index.php
+++ web/index.php
@@ -1,61 +1,51 @@
 <?php
-// Setup
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// REST API for Instagram
-// check if the input from the form is empty, if not store in _GET array
-if(!empty(_GET['location']))
-{
-    // url for the api, urlencode will encode any invalid characters that users input i.e. whitespace
-    $map_url = 'https://'.
-        'maps.googleapis.com/maps/api/geocode/json'. // google maps api interface
-        '?address='. urlencode($_GET['location']); // query statement for the api
-
-    // Processes the "GET" request and returns json. Note there are other methods for this function i.e. PUT POST
-    $map_json = file_get_contents($map_url);
-
-    // Now decode the json returned from the GET request into an array for users
-    $map_array = json_decode($map_json, true);
-
-    // Lets get the lat and long for the instagram api
+if (!empty($_GET['location'])) {
+    /**
+     * Here we build the url we'll be using to access the google maps api
+     */
+    $maps_url = 'https://' .
+        'maps.googleapis.com/' .
+        'maps/api/geocode/json' .
+        '?address=' . urlencode($_GET['location']);
+    $maps_json = file_get_contents($maps_url);
+    $maps_array = json_decode($maps_json, true);
     $lat = $maps_array['results'][0]['geometry']['location']['lat'];
     $lng = $maps_array['results'][0]['geometry']['location']['lng'];
-
-    $instagram_url = 'https://' .
-        'api.instagram.com/v1/media/search' . // instagram query statement
-        '?lat=' . $lat . // lat param for api
-        '&lng=' . $lng . // long param for api
-        '&client_id=812c81cd96b946c7a36d14d3373e85ccD';
-        // you will need to get an access token if you wish to deploy this yourself
-
-    // send GET request to get the images
-    $instagram_json = file_get_contents($url);
-
-    // decode the json and store in an array
-    $instagram_array = json_decode($json, true);
+    /**
+     * Time to make our Instagram api request. We'll build the url using the
+     * coordinate values returned by the google maps api
+     */
+    $url = 'https://' .
+        'api.instagram.com/v1/media/search' .
+        '?lat=' . $lat .
+        '&lng=' . $lng .
+        '&client_id=812c81cd96b946c7a36d14d3373e85cc'; //replace "CLIENT-ID"
+    $json = file_get_contents($url);
+    $array = json_decode($json, true);
 }
-
-// Our web handlers
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig',
-        array(
-            'instagram_imgs' => $instagram_array
-        )
-    );
-});
-
-$app->run();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>geogram</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+<form action="" method="get">
+    <input type="text" name="location"/>
+    <button type="submit">Submit</button>
+</form>
+<br/>
+<div id="results" data-url="<?php if (!empty($url)) echo $url ?>">
+    <?php
+    if (!empty($array)) {
+        foreach ($array['data'] as $key => $item) {
+            echo '<img id="' . $item['id'] . '" src="' . $item['images']['low_resolution']['url'] . '" alt=""/><br/>';
+        }
+    }
+    ?>
+</div>
+</body>
+</html>
