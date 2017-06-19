cf2cce35b341720d00b713362ab6d11dc4761125
Chris Womack <chris.d.womack@gmail.com>
2017-06-18 23:45:56 -0600

    added new index file

--- web/index.php
+++ web/index.php
@@ -29,16 +29,11 @@ if (!empty($_GET['location'])) {
 <head>
     <meta charset="utf-8"/>
     <title>geogram</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="script.js"></script>
 </head>
 <body>
 <form action="" method="get">
     <input type="text" name="location"/>
     <button type="submit">Submit</button>
-</form>
-<br/>
-<div id="results" data-url="<?php if (!empty($url)) echo $url ?>">
     <?php
     if (!empty($insta_array)) {
         foreach ($insta_array['data'] as $image) {
@@ -48,6 +43,6 @@ if (!empty($_GET['location'])) {
         }
     }
     ?>
-</div>
+</form>
 </body>
 </html>
